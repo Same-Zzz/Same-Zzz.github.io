538f894e1dfd9623f6af710f9798e6c815d249b1
Same H <13457183885@163.com>
2023-05-12 17:16:23 +0800

    510

---
+++ Same_bilibili/0.0.py
@@ -0,0 +1,10 @@
+"""
+ctrl + alt + s : 打开软件设置
+ctrl + d ：复制当前行代码
+shift + alt + 上\下 ： 将当前行代码上移或下移
+crtl + shift + f10 : 运行当前代码文件
+shift + f6 ：重命名文件
+ctrl + a : 全选
+ctrl + c\v\x : 复制、粘贴、剪切
+ctrl + f : 搜索
+"""
